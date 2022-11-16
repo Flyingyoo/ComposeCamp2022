328d50edbb3a6b5da4d91bc6f824ea7c7070644c
hsyoo <hsyoo0404@lifeplanet.co.kr>
2022-11-16 18:01:47 +0900

    damp 애니메이션 추가

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.codelab.basics
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 import com.codelab.basics.ui.theme.Yellow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.runtime.saveable.rememberSaveable
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +43,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+@Composable
 fun Greeting(name: String) {
-    var expended = false
+    var expended by rememberSaveable{ mutableStateOf(false) }
+    //val extraPadding = if(expended.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if(expended) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     val onClick = {
         expended = !expended
     }
@@ -38,29 +84,62 @@ fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
-                .padding(10.dp)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
                 .weight(1f)) {
                 Text(text = "sleepy...", Modifier.fillMaxWidth())
                 Text(text = "isn't you, $name?", Modifier.fillMaxWidth())
             }
-            Button(onClick = onClick, Modifier.background(Yellow).border(BorderStroke(1.dp, Color.Red))) {
-                Text(if(expended)"show less" else "show more")
+            ElevatedButton(onClick = onClick) {
+                Text(if(expended) "show less" else "show more")
             }
         }
 
     }
-
 }
 
 @Composable
 private fun FlyingApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf<String>("hsyoo", "flyingyoo")
+    modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.padding(4.dp)) {
-        for(name in names) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun FlyingAppPreview() {
+    BasicsCodelabTheme {
+        FlyingApp(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
+    }
+//    Column(modifier = modifier.padding(4.dp)) {
+//        for(name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview() {
+    BasicsCodelabTheme {
+        Greetings()
     }
 }
 
